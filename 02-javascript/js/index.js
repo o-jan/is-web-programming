5dc45d59c6a19c6f53f8483a0f2a969db408cd50
o-jan <fauzanmansur.a@gmail.com>
2021-10-05 07:51:25 +0800

    Tugas ke-2 versi 1

---
+++ 02-javascript/js/index.js
@@ -0,0 +1,50 @@
+const btn = document.getElementById("btn-add");
+const input = document.getElementById("shopping-input");
+const list = document.getElementById("shopping-list");
+
+const button = document.getElementById('initial')
+button.addEventListener("click", () => {button.parentElement.parentElement.remove();});
+
+function addList(eve) {
+	eve.preventDefault();
+	if (input.value == "") {
+		alert("It cannot be empty mate!");
+		return;
+	}
+
+	const entry = list.getElementsByTagName("LI");
+
+	for (let i = 0; i < entry.length; i++) {
+		if (entry[i].innerText === input.value) {
+			alert("There is a same thing here mate!");
+			input.focus();
+			return;
+		}
+	}
+
+	const node = document.createElement("li");
+
+	// left span
+	const leftSpan = document.createElement("span");
+	leftSpan.className = 'left';
+	const textnode = document.createTextNode(input.value + " ");
+	leftSpan.append(textnode);
+	node.appendChild(leftSpan);
+
+	// right side
+	const rightSpan = document.createElement("span");
+	rightSpan.className = 'right';
+	const x = document.createElement("BUTTON");
+	x.className = 'btn';
+	x.innerHTML = '<i class="fa fa-close"></i>';
+	x.addEventListener("click", () => {x.parentElement.parentElement.remove();});
+	rightSpan.append(x);
+	node.appendChild(rightSpan);
+	list.appendChild(node);
+
+	input.value = "";
+}
+
+
+input.focus();
+btn.addEventListener('click', addList);
